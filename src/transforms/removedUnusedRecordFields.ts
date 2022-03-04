1dabb607ea0713b8dc0108f76e526a14e20a3d76
Jeroen Engels <jfm.engels@gmail.com>
2022-03-04 18:10:32 +0100

    Prevent visiting things that should be avoided

--- src/transforms/removedUnusedRecordFields.ts
+++ src/transforms/removedUnusedRecordFields.ts
@@ -5,7 +5,10 @@ export const createRemoveUnusedRecordFieldsTransform: ts.TransformerFactory<ts.S
         const usedFields = new Set();
 
         const fieldsCollectorVisitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
-            if (ts.isPropertyAccessExpression(node) && ts.isIdentifier((node.name))) {
+            if (shouldBeAvoided(node)) {
+                return node;
+            }
+            else if (ts.isPropertyAccessExpression(node) && ts.isIdentifier((node.name))) {
                 usedFields.add(node.name.text);
             }
 
