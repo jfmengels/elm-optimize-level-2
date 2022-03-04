6f0eb4224a8fa48604debc2c3a33ec616767e7cb
Jeroen Engels <jfm.engels@gmail.com>
2022-03-04 17:55:39 +0100

    Removed registering of fields

--- src/transforms/removedUnusedRecordFields.ts
+++ src/transforms/removedUnusedRecordFields.ts
@@ -2,17 +2,11 @@ import ts from 'typescript';
 
 export const createRemoveUnusedRecordFieldsTransform: ts.TransformerFactory<ts.SourceFile> = context => {
     return sourceFile => {
-        const knownFields = new Set();
         const usedFields = new Set();
 
         const fieldsCollectorVisitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
             if (ts.isCallExpression(node) && ts.isIdentifier(node.expression) && node.expression.text === "_Platform_export") {
                 return node;
-            }
-            else if (ts.isObjectLiteralExpression(node)) {
-                node.properties.forEach((it) =>
-                    knownFields.add((it.name as ts.Identifier).text)
-                );
             } else if (ts.isPropertyAccessExpression(node) && ts.isIdentifier((node.name))) {
                 usedFields.add(node.name.text);
             }
